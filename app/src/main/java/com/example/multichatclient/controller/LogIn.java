ea03892339eb09dd968fb5662f8952f6926a179e
Trincalex <al.trincone@studenti.unina.it>
2022-12-29 13:02:26 +0100

    migliorata stringa Socket

--- app/src/main/java/com/example/multichatclient/controller/LogIn.java
+++ app/src/main/java/com/example/multichatclient/controller/LogIn.java
@@ -18,15 +18,13 @@ public class LogIn extends AsyncTask<String, Void, Void> {
     @Override
     protected Void doInBackground (String... voids) {
         try {
-            String message = voids[0] + "\r\n" + voids[1];
+            String message = "cmd=login\r\nnome=" + voids[0] + "\r\npassword=" + voids[1]+ "\r\n";
             s = new Socket ("192.168.1.3",10000);
             pw = new PrintWriter(s.getOutputStream());
             pw.write(message);
             pw.flush();
             pw.close();
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
