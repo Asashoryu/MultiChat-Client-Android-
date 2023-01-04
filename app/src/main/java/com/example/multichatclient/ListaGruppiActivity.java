69a2d41b9395620e11be529a4030fce330941d61
Trincalex <al.trincone@studenti.unina.it>
2023-01-04 12:44:09 +0100

    Aggiunta barra opzioni

--- app/src/main/java/com/example/multichatclient/ListaGruppiActivity.java
+++ app/src/main/java/com/example/multichatclient/ListaGruppiActivity.java
@@ -1,8 +1,14 @@
 package com.example.multichatclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,18 +18,33 @@ import java.util.Collections;
 public class ListaGruppiActivity extends AppCompatActivity {
 
     String[] prova;
-    final ArrayList<String> listp = new ArrayList<String>();
-    ListView mylist;
+    final ArrayList<String> aListp = new ArrayList<String>();
+    ListView vlist;
     ArrayAdapter<String> adapter;
+    //Toolbar optionToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_gruppi);
         prova = new String[] { "Product1", "Product2", "Product3" };
-        Collections.addAll(listp, prova);
-        mylist = (ListView) findViewById(R.id.listView1);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listp);
-        mylist.setAdapter(adapter);
+        Collections.addAll(aListp, prova);
+        vlist = (ListView) findViewById(R.id.listView1);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, aListp);
+        vlist.setAdapter(adapter);
+        vlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adattatore, View componente, int pos, long index) {
+                String elem = (String)adattatore.getItemAtPosition(pos);
+                Log.d ("List","Elemento clickato = " + elem);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
