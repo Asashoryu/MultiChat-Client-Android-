0ffa8735ec49dae47ef49286a66a6d77e904450b
Trincalex <al.trincone@studenti.unina.it>
2023-01-17 12:58:05 +0100

    Ciao Trincalex

--- app/src/main/java/com/mcc/multichatclone/controller/Controller.java
+++ app/src/main/java/com/mcc/multichatclone/controller/Controller.java
@@ -12,7 +12,7 @@ public class Controller {
     public Controller() {
         gruppi = new ArrayList<>();
         gruppi.add(new Gruppo("Prova 1"));
-        gruppi.add(new Gruppo("Ciao Trinca"));
+        gruppi.add(new Gruppo("Ciao Trincalex"));
 
         gruppoNavigato = null;
     }
