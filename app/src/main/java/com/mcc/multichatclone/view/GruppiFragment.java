4f3bd6868c027d333eed455ec8fa48858f6fb4b6
Trincalex <al.trincone@studenti.unina.it>
2023-01-18 18:05:55 +0100

    Migliorata grafica

--- app/src/main/java/com/mcc/multichatclone/view/GruppiFragment.java
+++ app/src/main/java/com/mcc/multichatclone/view/GruppiFragment.java
@@ -1,10 +1,7 @@
 package com.mcc.multichatclone.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
